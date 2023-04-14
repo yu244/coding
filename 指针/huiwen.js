f6ffc0506cfeee3d72508b5fc5cde92dcdeb5bd2
yyk <2793875028@qq.com>
2023-04-14 17:32:42 +0800

    最长回文

---
+++ 指针/huiwen.js
@@ -0,0 +1,15 @@
+// 待完善，双指针扩散
+var longestPalindrome = function(s) {
+  const len = s.length
+  const times = Math.floor(len / 2)
+  const list = s.split('')
+  let result = []
+  if (len & 1) {
+    // 奇数
+
+  } else {
+    // 偶数
+  }
+};
+
+ console.log('longestPalindrome', longestPalindrome('abcdaaa')) 
