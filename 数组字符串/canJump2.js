16532b474e391857bb35cbf49ca405c23097d604
yyk <2793875028@qq.com>
2023-11-17 18:00:53 +0800

    贪心，还有点问题

--- 数组字符串/canJump2.js
+++ 数组字符串/canJump2.js
@@ -23,6 +23,29 @@
  * @return {number}
  */
 // 贪心
+/*
+  问题归纳为 比较每一步的最大覆盖范围，历经最小 step 步，可到达终点
+  step 在什么情况下 + 1 步 ？
+  比如 第 i 阶的 最大覆盖范围 为 i + nums[i]，记作 nextIndex
+  那么在 i 到 nextIndex 之间，我们需要考虑下一步能够到达的 最远距离，
+  并更新 nextIndex, 当 i 走到 nextIndex 时，将 startIndex 改为 nextIndex 且 step + 1，代表这一步是无法避免的
+*/
+var jump3 = function(nums) {
+  let startIndex = 0
+  let nextIndex = 0
+  let step = 0
+  if (nums.length === 1) return 0
+  for (let i = 0; i < nums.length; i++) {
+    if (i === nextIndex) {
+      startIndex = nextIndex
+      step++
+    }
+    nextIndex = Math.max(nums[i] + i, nextIndex)
+  }
+  return step
+}
+
+// 别人的贪心写法
 var jump = function(nums) {
   let res = 0, start = 0, end = 1
   while (end < nums.length) {
@@ -36,3 +59,26 @@ var jump = function(nums) {
   }
   return res
 };
+
+// dp
+/*
+  计 到达 i 阶台阶的最小跳跃数为 dp[i]
+  假设第 j 阶台阶能够到达 i，也就是 （ num[j] + j >= i )时,
+  dp[i] = Math.min(dp[j] + 1, dp[i])
+*/
+var jump2 = function(nums) {
+  let n = nums.length
+  let dp = new Array(n).fill(Infinity)
+  dp[0] = 0
+  for (let i = 1; i < n; i++) {
+    for (let j = 0; j < i; j++) {
+      if (nums[j] + j >= i) {
+        dp[i] = Math.min(dp[i], dp[j] + 1)
+      }
+    }
+  }
+  return dp[n - 1]
+}
+
+const nums = [2,3,0,1,4]
+console.log('jump3(nums)', jump3(nums))
