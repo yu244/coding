51fde30e6c252449e399089bb380d9b735dba486
yyk <2793875028@qq.com>
2023-12-08 13:04:24 +0800

    单指针+hashMap基本过测，超时

--- 滑动窗口/minWindow.js
+++ 滑动窗口/minWindow.js
@@ -25,20 +25,52 @@
  * @param {string} t
  * @return {string}
  */
+// 单指针每次重建 map 解法，265 / 267 超时
 var minWindow = function(s, t) {
-  let tMap = {}
-  for (const item of t) {
-    tMap[item] = []
+  let tMap = new Map()
+  let right = 1
+  let temp = Infinity
+  let res = []
+  if (s.length === 0) return ''
+  if (s === t) return t
+  for (let i = 0; i < t.length; i++) {
+    tMap.set(t[i], (tMap.get(t[i]) || 0) + 1)
   }
   for (let i = 0; i < s.length; i++) {
-    if (tMap[s[i]]) {
-      tMap[s[i]].push(i)
+    if (tMap.get(s[i])) {
+      let curMap = new Map(tMap)
+      curMap.set(s[i], curMap.get(s[i]) - 1)
+      if (curMap.get(s[i]) === 0) {
+        curMap.delete(s[i])
+      }
+      if (curMap.size === 0) {
+        return s[i]
+      }
+      right = i + 1
+      while (right < s.length) {
+        if (curMap.has(s[right])) {
+          curMap.set(s[right], curMap.get(s[right]) - 1)
+          if (curMap.get(s[right]) === 0) {
+            curMap.delete(s[right])
+          }
+        }
+        if (curMap.size === 0) {
+          if (right - i < temp) {
+            temp = right - i
+            res = [i, right]
+          }
+          break
+        }
+        right++
+      }
     }
   }
-  let arr = []
-  
+  if (res.length > 0) {
+    return s.slice(res[0], res[1] + 1)
+  }
+  return ''
 };
 
-let s = "ADOBECODEBANC", t = "ABC"
+let s = "ab", t = "a"
 
 console.log('minWindow(s, t)', minWindow(s, t))
