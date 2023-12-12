cfa5fe713955c9e92b48655d9ad608ef2d007c87
yyk <2793875028@qq.com>
2023-12-12 14:21:00 +0800

    有效数独

--- 矩阵/isValidSudoku.js
+++ 矩阵/isValidSudoku.js
@@ -41,7 +41,33 @@
  * @return {boolean}
  */
 var isValidSudoku = function(board) {
+  const rows = new Array(9).fill(0).map(() => new Array(9).fill(0))
+  const columns = new Array(9).fill(0).map(() => new Array(9).fill(0))
   const subboxes = new Array(3).fill(0).map(() => new Array(3).fill(0).map(() => new Array(9).fill(0)));
-  console.log('subboxes', subboxes)
+  const base = '0'.charCodeAt() + 1
+  console.log('base', base)
+  for (let i = 0; i < 9; i++) {
+    for (let j = 0; j < 9; j++) {
+      if (board[i][j] !== '.') {
+        let index = board[i][j].charCodeAt() - base
+        rows[i][index] += 1
+        columns[j][index] += 1
+        subboxes[Math.floor(i / 3)][Math.floor(j / 3)][index] += 1
+        if (rows[i][index] > 1 || columns[j][index] > 1 || subboxes[Math.floor(i / 3)][Math.floor(j / 3)][index] > 1) return false
+      }
+    }
+  }
+  return true
 };
-isValidSudoku()
+const board = 
+  [["8","3",".",".","7",".",".",".","."]
+  ,["6",".",".","1","9","5",".",".","."]
+  ,[".","9","8",".",".",".",".","6","."]
+  ,["8",".",".",".","6",".",".",".","3"]
+  ,["4",".",".","8",".","3",".",".","1"]
+  ,["7",".",".",".","2",".",".",".","6"]
+  ,[".","6",".",".",".",".","2","8","."]
+  ,[".",".",".","4","1","9",".",".","5"]
+  ,[".",".",".",".","8",".",".","7","9"]]
+
+console.log('isValidSudoku(board)', isValidSudoku(board))
