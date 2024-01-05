bdf4a4bb37f37ca4518a440d689da8ab817c6ee4
yyk <2793875028@qq.com>
2024-01-05 11:48:14 +0800

    二叉树展开为链表 解法2

--- 二叉树/flatten.js
+++ 二叉树/flatten.js
@@ -27,6 +27,7 @@
  * @param {TreeNode} root
  * @return {void} Do not return anything, modify root in-place instead.
  */
+// 找到左子树的最右节点，将root右子树转接过来，再将root下沉
 var flatten = function(root) {
   while (root !== null) {
     if (root.left === null) {
@@ -42,4 +43,30 @@ var flatten = function(root) {
       root = root.right
     }
   }
-};
+}
+// 通过 变形的后序遍历 + 队列 的方式从尾部开始，找到右子树的最右节点开始 合并左子树最右节点
+// 先序 DLR、中序 LDR、后序 LRD
+var flatten = function(root) {
+  const stack = new Array()
+  let cur = root
+  let pre = null
+
+  while (cur !== null || stack.length !== 0) {
+    while (cur !== null) {
+      stack.push(cur)
+      cur = cur.right
+    }
+    cur = stack[stack.length - 1]
+    // 在不存在左节点或右节点已经访问过的情况下，访问根节点
+    if (cur.left === null || cur.left === pre) {
+      stack.pop()
+      cur.right = pre
+      cur.left = null
+      pre = cur
+      cur = null
+    } else {
+    // 左节点还没访问过就先访问左节点
+      cur = cur.left
+    }
+  }
+}
