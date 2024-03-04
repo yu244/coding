43f310604ccc9693e20bc2cd1c409c9250016bf9
yyk <2793875028@qq.com>
2024-03-04 17:45:13 +0800

    一道链表重新做

--- 链表/hasCycle.js
+++ 链表/hasCycle.js
@@ -42,3 +42,15 @@ var hasCycle = function(head) {
   }
   return result
 }
+// 注意判断位置 true 需要在最后
+var hasCycle = function(head) {
+  if (head === null) return false
+  let fast = head
+  let slow = head
+  while(fast !== null && fast.next !== null) {
+    fast = fast.next.next
+    slow = slow.next
+    if (fast === slow) return true
+  }
+  return false
+}
